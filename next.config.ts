7032b32032f12d6603786588f23c109304353ea8
DevNahyanK <nahyangaming789@gmail.com>
2025-01-22 01:55:59 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+      
+//         hostname: "cdn.sanity.io", // Ensure this matches your image domain
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-      
-        hostname: "cdn.sanity.io", // Ensure this matches your image domain
-      },
-    ],
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
   },
 };
 
