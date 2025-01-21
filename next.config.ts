013b0663b58016f6d5571e8b54287f2c4ab9afea
DevNahyanK <nahyangaming789@gmail.com>
2025-01-22 02:04:35 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.sanity.io"], 
+    
+  },
+};
+  module.exports = nextConfig;
+
+
+
+  
+// export default nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-      
-//         hostname: "cdn.sanity.io", // Ensure this matches your image domain
-//       },
-//     ],
+//   env: {
+//     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+//     SANITY_DATASET: process.env.SANITY_DATASET,
+//     SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
 //   },
 // };
 
 // export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
-    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
-  },
-};
-
-export default nextConfig;
